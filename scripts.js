2d0bce8fdf5b20f21541c3cc3d9279d417ef91f1
vulkan-blue <franksmherrera@gmail.com>
2024-12-18 23:44:11 -0300

    upgraded

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+function mostrarAlerta() {
+    // Obtener el valor del textarea
+    var texto = document.getElementById('miTextarea').value;
+    
+    // Mostrar el valor en una alerta
+    if (texto == 23){
+    alert("contraseña correcta");
+    
+
+}window.location.href = 'grupovb.html'
+}
+
+
